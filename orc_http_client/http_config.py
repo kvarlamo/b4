9efdf97041f149b6423d688c048471bc75b0c554
Kirill Varlamov <k.varlamov@brain4net.com>
2016-03-28 00:51:46 +0300

    Added switches activation and renaming

--- orc_http_client/http_config.py
+++ orc_http_client/http_config.py
@@ -49,6 +49,12 @@ class CtlAPI:
         self.r = self.client.get(self.baseurl + self.url, headers={"content-type": "application/json;charset=utf-8", 'X-CSRF-TOKEN': self.csrf})
         print("HTTP_Resp:%s %s" % (self.r.status_code, self.r.text))
         return(self.r.json())
+    def put(self, url, request):
+        self.url = url
+        self.request = json.dumps(request).encode('utf8')
+        self.get_csrf_token()
+        print("PUT %s%s %s " % (self.baseurl, self.url, self.request), end="")
+        self.r = self.client.put(self.baseurl + self.url, self.request, headers={"content-type": "application/json;charset=utf-8", 'X-CSRF-TOKEN': self.csrf})
     def get_csrf_token(self):
         print("GET CSRF Token at %s " % self.baseurl, end="")
         self.r = self.client.get(self.baseurl)
@@ -57,7 +63,7 @@ class CtlAPI:
         print("Token:%s" % self.csrf)
 
 c = CtlAPI('http://10.7.1.101:8080/', 'admin', 'admin')
-
+"""
 # get all clusters and switches
 clusters=c.get('api/clusters')
 if (clusters):
@@ -87,14 +93,19 @@ c.post('api/controllers',{"name":None,"ip":None,"id":None,"jgroupsPort":7800,"ne
 c.post('api/controllers',{"name":None,"ip":None,"id":None,"jgroupsPort":7800,"netconfPort":830,"address":"ctl3"})
 
 # Wait untill all the controllers get connected
-#TBD
+while True:
+    controllers=c.get('api/controllers')
+    print(controllers)
+    if (len(controllers) == 3):
+        break
+    time.sleep(5)
 
 # GET configured controllers and
 # ADD them to the redundant cluster
 controllers=c.get('api/controllers')
 clust={"name":"Cluster1","ip":None,"clusterType":"MASTER_SLAVE","id":None,"nodes":controllers, "masterNode": controllers[1],"tagType":"STag_VLAN"}
 c.post('api/clusters',clust)
-
+"""
 # Wait until cluster[0] become Active
 while True:
     cluster=c.get('api/clusters')[0]
@@ -105,11 +116,31 @@ while True:
 
 #Wait until all 4 switches become visible
 while True:
-    switches=c.get('api/cluster/%s/commutators&page=1&size=15' % cluster['id'])
-    print("%s switches in cluster" % len(switches))
-    if (len(switches) == 4):
+    switches=c.get('api/cluster/%s/commutators?page=1&size=15' % cluster['id'])
+    print("%s switches in cluster" % switches['totalSize'])
+    if (switches['totalSize'] == 4):
         break
     time.sleep(5)
 
-for sw in switches:
-    print(sw)
+# Enable all inactive switches
+for i in range(len(switches['content'])):
+    if (switches['content'][i]['status'] == 'INACTIVE'):
+        c.put("api/cluster/%s/commutators/enable/%s" % (cluster['id'],switches['content'][i]['id']), None)
+
+# Show switches' MAC addresses
+# Useful to copy-paste below for renaming
+print("Switch MAC addresses are:")
+for i in range(len(switches['content'])):
+    print(switches['content'][i]['mac'])
+
+# switch names to MAC mapping for renaming
+sw_names={'00:e0:ed:2f:51:f8': 'HQ',
+          '00:e0:ed:2a:74:e6': 'BR1',
+          '00:e0:ed:2e:4e:e0': 'BR2',
+          '00:e0:ed:2f:52:04': 'BR3'}
+
+# rename switches which have MAC equal to name
+for i in range(len(switches['content'])):
+    if (switches['content'][i]['name'] == switches['content'][i]['mac']):
+        switches['content'][i]['name'] = sw_names[switches['content'][i]['mac']]
+        c.put("api/cluster/%s/commutators" % cluster['id'], switches['content'][i])
