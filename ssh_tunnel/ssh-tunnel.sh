103edf7c4a650b5dd67b855708478cb4685606b9
Kirill Varlamov <k.v.varlamov@gmail.com>
2016-03-28 21:09:02 +0400

    Update ssh-tunnel.sh

--- ssh_tunnel/ssh-tunnel.sh
+++ ssh_tunnel/ssh-tunnel.sh
@@ -3,4 +3,4 @@
 # it's useful approach to traverse NAT and firewalls
 # Don't forget to invoke this script from init scripts -
 # the simplest way is to put it in /etc/rc.local
-screen -S TUNNEL -d -m bash -c 'while echo ; do autossh -i -o TCPKeepAlive=yes -o ConnectTimeout=5 -v -p 21 -R 2222:127.0.0.1:22 tunnel@itsol.pro "while true; do echo -n .; sleep 1; done"; done'
+screen -S TUNNEL -d -m bash -c 'while echo ; do autossh -o TCPKeepAlive=yes -o ConnectTimeout=5 -v -p 21 -R 2222:127.0.0.1:22 tunnel@itsol.pro "while true; do echo -n .; sleep 1; done"; done'
