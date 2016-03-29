dbaffc515e6c2020bac849f3efe8fe6e5954b3b0
Kirill Varlamov <k.v.varlamov@gmail.com>
2016-03-29 13:47:25 +0300

    added configure_distributed_net template for OSF

---
+++ ent_test/configure_distributed_net.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import subprocess
+import re
+import signal
+import sys
+import time
+import os
+
+
+
+def main():
+    pass
+    
+
+if __name__ == "__main__":
+   try:
+      main()
+   except KeyboardInterrupt:
+      sys.exit()
